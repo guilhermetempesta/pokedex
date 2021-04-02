05de4ab3a8e0bb7b1c26cbcda2c6257d21f25162
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2021-04-02 17:29:44 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/pokemon.png">
+      <hr>
+      <input class="input is-rounded" type="text" placeholder="Buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-rounded is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: async function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
+      console.log("pegou a lista de pokemons");
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    })
+  },
   components: {
-    HelloWorld
+    Pokemon
+  },
+  methods: {
+    buscar: function() {
+      if (this.busca.trim() == '') {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.toUpperCase() == this.busca.toUpperCase());
+      }
+    }
+  },
+  computed: {
+    resultadoBusca: function(){
+      if (this.busca.trim() == '') {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter(pokemon => pokemon.name.toUpperCase() == this.busca.toUpperCase());
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  background-color: rgb(252, 248, 248);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#buscaBtn {
+  margin-top: 2%;
 }
 </style>
